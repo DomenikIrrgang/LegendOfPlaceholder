e7fb2e0241ab149057f3c377b9bee73e3b508a91
Domenik Irrgang <domenikirrgang@gmail.com>
2023-11-30 12:10:08 +0100

    feat: added boss percentage label

--- ui/GameUI/Bossframe.gd
+++ ui/GameUI/Bossframe.gd
@@ -11,7 +11,10 @@ var boss_level_label: Label = $Frame/FrameTexture/CenterLevel/BossLevel
 var boss_name_label: Label = $Frame/FrameTexture/CenterName/BossName
 
 @onready
-var boss_health_label: Label = $Frame/FrameTexture/HealthBar/CenterHealthText/BossHealth
+var boss_health_label: Label = $Frame/FrameTexture/HealthBar/BossHealth/Label
+
+@onready
+var boss_health_percentage_label: Label = $Frame/FrameTexture/HealthBar/BossHealthPercenate/Label
 
 @onready
 var castbar: Castbar = $Castbar
@@ -35,7 +38,8 @@ func on_encounter_started(boss: Unit) -> void:
 	boss_name_label.text = boss.get_alias()
 	health_bar.max_value = boss.get_resource(ResourceType.Enum.HEALTH).get_maximum_value()
 	health_bar.value = boss.get_resource(ResourceType.Enum.HEALTH).get_value()
-	boss_health_label.text = str(boss.get_resource(ResourceType.Enum.HEALTH).get_value()) + " / " + str(boss.get_resource(ResourceType.Enum.HEALTH).get_maximum_value())
+	boss_health_label.text = get_boss_health_string(boss.get_resource(ResourceType.Enum.HEALTH))
+	boss_health_percentage_label.text = get_boss_health_percentage_string(boss.get_resource(ResourceType.Enum.HEALTH))
 	boss.get_resource(ResourceType.Enum.HEALTH).resource_value_changed.connect(on_boss_health_changed)
 	boss.get_resource(ResourceType.Enum.HEALTH).resource_maximum_value_changed.connect(on_boss_max_health_changed)
 	castbar.initialize(boss)
@@ -43,16 +47,24 @@ func on_encounter_started(boss: Unit) -> void:
 	debuffs.initialize(boss)
 	visible = true
 	
+func get_boss_health_string(resource: UnitResource) -> String:
+	return str(resource.get_value()) + " / " + str(resource.get_maximum_value())
+	
+func get_boss_health_percentage_string(resource: UnitResource) -> String:
+	return str(snapped(float(resource.get_value()) / float(resource.get_maximum_value()) * 100, 0.1)) + "%"
+	
 func on_boss_health_changed(resource: UnitResource, _new_value: int, _change: int, _original_change: int) -> void:
 	tween.kill()
 	tween = create_tween()
 	tween.tween_property(health_bar, "value", resource.get_value(), 0.2)
 	tween.play()
-	boss_health_label.text = str(resource.get_value()) + " / " + str(resource.get_maximum_value())	
+	boss_health_label.text = get_boss_health_string(resource)
+	boss_health_percentage_label.text = get_boss_health_percentage_string(resource)
 	
 func on_boss_max_health_changed(resource: UnitResource, _new_maximum_value: int) -> void:
 	health_bar.max_value = resource.get_maximum_value()
-	boss_health_label.text = str(resource.get_value()) + " / " + str(resource.get_maximum_value())	
+	boss_health_label.text = get_boss_health_string(resource)
+	boss_health_percentage_label.text = get_boss_health_percentage_string(resource)
 	
 func on_encounter_ended() -> void:
 	visible = false
