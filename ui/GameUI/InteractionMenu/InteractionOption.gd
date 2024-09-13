a8e83cb5b590a6d8a91c9c601e7fad35a8e39e4e
Domenik Irrgang <domenikirrgang@gmail.com>
2024-09-13 18:42:15 +0200

    feat: made selection npc interaction options list more visible

--- ui/GameUI/InteractionMenu/InteractionOption.gd
+++ ui/GameUI/InteractionMenu/InteractionOption.gd
@@ -10,12 +10,17 @@ var label: Label = $Margin/Spacing/Label
 @onready
 var background: NinePatchRect = $NinePatchRect
 
+@onready
+var shade: ColorRect = $Shade
+
 func init(interaction: Interaction) -> void:
 	icon.texture = interaction.get_icon()
 	label.text = interaction.get_alias()
 
 func select() -> void:
-	background.material.set_shader_parameter("enabled", 0.0)
+	shade.visible = false
+	#background.material.set_shader_parameter("enabled", 0.0)
 	
 func unselect() -> void:
-	background.material.set_shader_parameter("enabled", 1.0)
+	shade.visible = true
+	#background.material.set_shader_parameter("enabled", 1.0)
