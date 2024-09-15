3e018988bf677b3210051ca8352599b024a5d107
Domenik Irrgang <domenikirrgang@gmail.com>
2024-09-15 17:16:54 +0200

    feat: focus respawn on death

--- ui/DeathScreen/Respawn.gd
+++ ui/DeathScreen/Respawn.gd
@@ -1,5 +1,12 @@
 extends Button
 
+func _ready() -> void:
+	visibility_changed.connect(on_visibility_changed)
+	
+func on_visibility_changed() -> void:
+	if visible:
+		grab_focus()
+
 func _on_pressed():
 	Globals.get_tree().paused = false
 	Globals.get_player().respawn()
