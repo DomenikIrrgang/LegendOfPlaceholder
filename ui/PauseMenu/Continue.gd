874ef66623c008ad2e03bd19c531dd53fa6d6e21
Domenik Irrgang <domenikirrgang@gmail.com>
2024-09-15 17:11:57 +0200

    feat: pause menu focus

--- ui/PauseMenu/Continue.gd
+++ ui/PauseMenu/Continue.gd
@@ -2,8 +2,12 @@ extends Button
 
 func _ready() -> void:
 	pressed.connect(on_pressed)
+	visibility_changed.connect(on_visiblity_changed)
 	
 func on_pressed() -> void:
 	Globals.get_user_interface().get_node("PauseMenu").visible = false
 	Globals.get_tree().paused = false
 	
+func on_visiblity_changed() -> void:
+	if visible:
+		grab_focus()
--- ui/PauseMenu/Settings.gd
+++ ui/PauseMenu/Settings.gd
@@ -5,4 +5,10 @@ func _ready():
 	
 func on_pressed() -> void:
 	var settings = Globals.get_scene_tree().get_first_node_in_group("Settings")
+	settings.hidden.connect(on_settings_hidden)
 	settings.visible = not settings.visible
+	
+func on_settings_hidden() -> void:
+	grab_focus()
+	var settings = Globals.get_scene_tree().get_first_node_in_group("Settings")
+	settings.hidden.disconnect(on_settings_hidden)
