a53b7071a91c6f59572e667b2ddda298dcd6836b
Domenik Irrgang <domenikirrgang@gmail.com>
2024-09-20 12:43:19 +0200

    feat: addec close button

--- ui/GameUI/Runesmithing/CraftButton.gd
+++ ui/GameUI/Runesmithing/CraftButton.gd
@@ -2,7 +2,10 @@ extends Button
 
 func _ready() -> void:
 	pressed.connect(on_pressed)
-	
+	RunesmithingManager.recipe_learned.connect(func(recipe: Recipe):
+		visible = RunesmithingManager.get_recipes().size() > 0
+	)
+	visible = RunesmithingManager.get_recipes().size() > 0
 	
 func on_pressed() -> void:
 	var recipe = Globals.get_tree().get_first_node_in_group("Recipelist").selected_recipe
--- ui/GameUI/Runesmithing/RecipeList.gd
+++ ui/GameUI/Runesmithing/RecipeList.gd
@@ -5,34 +5,39 @@ signal recipe_selected(recipe: Recipe)
 var selected_recipe: Recipe = null
 
 func _ready() -> void:
-	for recipe in RunesmithingManager.get_recipes():
-		add_recipe(recipe)
 	RunesmithingManager.recipe_learned.connect(add_recipe)
 	visibility_changed.connect(on_visibility_changed)
 	
 func on_visibility_changed() -> void:
-	if visible and get_child_count() > 0:
-		get_children()[0].grab_focus()
-		
+	if is_visible_in_tree():
+		%NoRecipes.visible = RunesmithingManager.get_recipes().size() == 0
+		for recipe in RunesmithingManager.get_recipes():
+			add_recipe(recipe)
+		if RunesmithingManager.get_recipes().size() >= 1:
+			%RecipeDetails.visible = true
+			get_children()[0].grab_focus()
+		else:
+			%RecipeDetails.visible = false
+			%CloseButton.grab_focus()
+	else:
+		Globals.free_children(self)
 				
 func add_recipe(recipe: Recipe) -> void:
-	var list_entry = Button.new()
-	list_entry.text = recipe.name
-	list_entry.icon = recipe.results[0].item.icon
-	list_entry.alignment = HORIZONTAL_ALIGNMENT_LEFT
-	list_entry.focus_entered.connect(func():
-		selected_recipe = recipe
-		recipe_selected.emit(recipe)
-	)
-	list_entry.pressed.connect(func():
-		RunesmithingManager.craft_recipe(recipe)
-	)
-	list_entry.button_mask = 0
-	add_child(list_entry)
-	if visible:
-		get_children()[get_children().size() - 1].grab_focus()
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if is_visible_in_tree():
+		var list_entry = Button.new()
+		list_entry.text = recipe.name
+		list_entry.icon = recipe.results[0].item.icon
+		list_entry.alignment = HORIZONTAL_ALIGNMENT_LEFT
+		list_entry.focus_entered.connect(func():
+			selected_recipe = recipe
+			recipe_selected.emit(recipe)
+		)
+		list_entry.pressed.connect(func():
+			RunesmithingManager.craft_recipe(recipe)
+		)
+		list_entry.button_mask = 0
+		add_child(list_entry)
+		var child_count =  get_child_count() 
+		if get_child_count() == 1:
+			%RecipeDetails.visible = true
+			%NoRecipes.visible = false
