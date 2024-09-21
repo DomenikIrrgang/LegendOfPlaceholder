94a214d32770bfa992edab0770afbb586b7f3ec8
Domenik Irrgang <domenikirrgang@gmail.com>
2024-09-21 08:54:42 +0200

    fix: resolved experiencebar for runesmithing not displaying correct values after loading from file

--- ui/GameUI/Runesmithing/ExperienceBar.gd
+++ ui/GameUI/Runesmithing/ExperienceBar.gd
@@ -6,13 +6,13 @@ var speed = 1.5
 func _ready() -> void:
 	RunesmithingManager.experience_gained.connect(on_experience_gained)
 	RunesmithingManager.experience_set.connect(func(amount: int):
-		value = amount
 		level = RunesmithingManager.level
-		max_value = RunesmithingManager.get_experience_needed_for_level_up(level)
+		max_value = RunesmithingManager.get_experience_needed_for_level(level)
+		value = RunesmithingManager.experience
 	)
 	level = RunesmithingManager.level
-	value = RunesmithingManager.experience
 	max_value = RunesmithingManager.get_experience_needed_for_level(level)
+	value = RunesmithingManager.experience
 	
 func update_experience_amount() -> void:
 	if level == RunesmithingManager.level:
@@ -39,4 +39,8 @@ func get_normalized_speed(target_value: float) -> float:
 	return speed * ((target_value - value) / max_value)
 	
 func on_experience_gained(amount: int) -> void:
+	var old_value = value
+	var preview_value = amount
+	var old_experience =  RunesmithingManager.experience
+	var total_xp = RunesmithingManager.get_experience_needed_for_level(RunesmithingManager.level)
 	update_experience_amount()
--- ui/GameUI/Runesmithing/ExperienceBarPreview.gd
+++ ui/GameUI/Runesmithing/ExperienceBarPreview.gd
@@ -3,6 +3,10 @@ extends TextureProgressBar
 func _ready() -> void:
 	RunesmithingManager.experience_gained.connect(on_experience_gained)
 	Globals.get_tree().get_first_node_in_group("Recipelist").recipe_selected.connect(on_recipe_selected)
+	RunesmithingManager.experience_set.connect(func(amount: int):
+		max_value = RunesmithingManager.get_experience_needed_for_level(RunesmithingManager.level)
+		value = amount
+	)
 	RunesmithingManager.recipe_crafted.connect(on_recipe_crafted)
 	update_experience_amount()
 	
@@ -10,11 +14,15 @@ func on_recipe_crafted(recipe: Recipe) -> void:
 	value = RunesmithingManager.experience + recipe.get_experience()
 	
 func on_recipe_selected(recipe: Recipe) -> void:
+	var old_value = value
+	var preview_value = recipe.get_experience()
+	var old_experience =  RunesmithingManager.experience
+	var total_xp = RunesmithingManager.get_experience_needed_for_level(RunesmithingManager.level)
 	value = RunesmithingManager.experience + recipe.get_experience()
 
 func update_experience_amount() -> void:
-	value = RunesmithingManager.experience
 	max_value = RunesmithingManager.get_experience_needed_for_level(RunesmithingManager.level)
+	value = RunesmithingManager.experience
 	
 func on_experience_gained(amount: int) -> void:
 	update_experience_amount()
